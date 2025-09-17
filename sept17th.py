2f82ef14a36ac6b1edb3556444d94525303a57f9
Sulaiman <Mohammedsulaimana9@gmail.com>
2025-09-17 19:30:41 +0530

    uploading my 2nd code

--- sept17th.py
+++ sept17th.py
@@ -8,3 +8,15 @@ B_left = B-G
 
 skills = (G*5)+(R_left*1)+(B_left*2)
 print(skills)
+
+#code 2
+# cook your dish here
+Testcases = int(input("Enter the number of testcases: "))
+for i in range(Testcases):
+    x = int(input())
+    if(x<=100):
+        print(x)
+    elif(x>100):
+        y = x-10
+        print(y)
+    
